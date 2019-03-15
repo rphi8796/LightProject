fb7edb4008781c586216a890ac0b8755b431e877
rphi8796 <jlkfop@gmail.com>
2019-03-15 10:00:06 -0600

    I added the rest of the buttons and positioned them. now they need actionListeners.

--- src/view/LightPanel.java
+++ src/view/LightPanel.java
@@ -16,10 +16,32 @@ public class LightPanel extends JPanel
 		super();
 		this.appController = appController;
 		
+		resetButton = new JButton("Reset");
+		appLayout = new SpringLayout();
+		appLayout.putConstraint(SpringLayout.SOUTH, resetButton, -67, SpringLayout.SOUTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, resetButton, -205, SpringLayout.EAST, this);
 		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setPreferredSize(new Dimension(800, 800));
+		this.add(resetButton);
+	}
+	
+	private void setupLayout()
+	{
 		
 	}
 	
+	private void setupListeners()
+	{
+		
+	}
 	
 	
 }
